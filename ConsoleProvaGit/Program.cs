ff9e52f124ef72d2a1d95ab1b648ec14ead53276
EnrAnt <enrico.antolini@outlook.com>
2021-06-08 15:34:45 +0200

    Commit2

--- ConsoleProvaGit/Program.cs
+++ ConsoleProvaGit/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleProvaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Prova1");
+            Console.WriteLine("Prova2");
         }
     }
 }
